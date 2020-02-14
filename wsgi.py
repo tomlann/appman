e6cbb4d4681670d2eea0f8a205abe3b9d7d5796b
tomlann <48773862+tomlann@users.noreply.github.com>
2020-02-14 12:12:14 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-import os 
-
 from project import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True,host='0.0.0.0')

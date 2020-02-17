641174133ae56bacc4cdf3829f38e9cbf7136ce5
tomlann <48773862+tomlann@users.noreply.github.com>
2020-02-17 09:07:52 -0600

    Update auth.py

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -7,9 +7,10 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():

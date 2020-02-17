03a8598812516f3d4d064e314756dc832bacc7e5
tomlann <48773862+tomlann@users.noreply.github.com>
2020-02-17 12:04:54 -0600

    Update auth.py

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
 from .models import User
 from . import db
 
@@ -25,6 +26,7 @@ def login_post():
         return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
